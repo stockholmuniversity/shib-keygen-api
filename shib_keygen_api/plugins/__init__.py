a313542deb91dae239a4fc290db4103fb3768be1
Simon Lundström <simlu+github@su.se>
2025-08-28 18:34:00 +0200

    Strip / to be able to add paths
    pathlib is weird

--- shib_keygen_api/plugins/__init__.py
+++ shib_keygen_api/plugins/__init__.py
@@ -13,6 +13,13 @@ class CSR(metaclass=ABCMeta):
     common_name: str
     path: str
 
+    # https://docs.python.org/3.11/library/pathlib.html#operators
+    # Apparently it's a feature that Path() from pathlib ignores previous
+    # path when merging to paths and the latter "is absolute" i.e. has a /
+    # first. This is not a great place to fix it, but an easy one.
+    def __post_init__(self) -> None:
+        self.path = self.path.lstrip("/")
+
 
 class Plugin(metaclass=ABCMeta):
     @classmethod
